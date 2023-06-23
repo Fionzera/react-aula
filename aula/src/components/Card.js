98e4d5c4456e9b353e908be649dc77d526ddcd05
Lucas de Lima <fionzera@gmail.com>
2023-06-23 11:56:56 -0300

    Aula 03

---
+++ aula/src/components/Card.js
@@ -0,0 +1,11 @@
+import './Card.css'
+
+const Card = () => {
+    return (
+        <div callName='cardContainer'>
+            <p>Meu primeiro componente em React</p>
+        </div>
+    );
+}
+
+export default Card;
